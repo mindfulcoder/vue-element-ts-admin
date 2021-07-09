d104cf42acabc29e42fbde6875d17173d5ac6018
Harry Chen <artofcode@qq.com>
2021-07-09 15:28:45 +0800

    extract Menu as standalone component

---
+++ src/components/Menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <template v-if="'children' in menu">
+    <el-submenu :index="menu.index" :key="menu.index">
+      <template #title>
+        <i :class="menu.icon"></i>
+        <span>{{ menu.title }}</span>
+      </template>
+      <Submenu v-for="child in menu.children" :menu="child"/>
+    </el-submenu>
+  </template>
+  <template v-else>
+    <el-menu-item :index="menu.index" :key="menu.index">
+      <i :class="menu.icon"></i>
+      <template #title>{{ menu.title }}</template>
+    </el-menu-item>
+  </template>
+</template>
+<script lang="ts">
+import {defineComponent} from "vue";
+import Submenu from "/@/components/Submenu.vue";
+
+export default defineComponent({
+  name: "Menu",
+  props: {
+    menu: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    Submenu
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,50 +1,42 @@
 <template>
   <div class="sidebar">
-    <el-menu class="sidebar-el-menu" :default-active="route.path" :collapse="store.state.collapse"
+    <el-menu class="sidebar-el-menu"
+             :default-active="route.path"
+             :collapse="store.state.collapse"
              background-color="#324157"
-             text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
-      <template v-for="menu in menuList">
-        <template v-if="menu.children">
-          <el-submenu :index="menu.index" :key="menu.index">
-            <template #title>
-              <i :class="menu.icon"></i>
-              <span>{{ menu.title }}</span>
-            </template>
-            <Submenu v-for="child in menu.children" :menu="child"/>
-          </el-submenu>
-        </template>
-        <template v-else>
-          <el-menu-item :index="menu.index" :key="menu.index">
-            <i :class="menu.icon"></i>
-            <template #title>{{ menu.title }}</template>
-          </el-menu-item>
-        </template>
-      </template>
+             text-color="#bfcbd9"
+             active-text-color="#20a0ff"
+             unique-opened
+             router>
+      <Menu v-for="menu in menuList" :menu="menu"/>
     </el-menu>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import {defineComponent} from "vue";
 import {useStore} from "vuex";
 import {useRoute} from "vue-router";
-import Submenu from "./Submenu.vue";
-import {static_routes} from "../router";
-import {getMenuList} from "../utils/menu";
+import Submenu from "/@/components/Submenu.vue";
+import {static_routes} from "/@/router";
+import {getMenuList} from "/@/utils/menu";
+import {getCurrentUser} from "/@/utils/user";
+import Menu from "/@/components/Menu.vue";
 
-export default {
-  components: {Submenu},
+export default defineComponent({
+  name: "Sidebar",
+  components: {Menu, Submenu},
   setup() {
-    const menuList = getMenuList(static_routes, ['admin']);
-    console.log(menuList)
-    const route = useRoute();
     const store = useStore();
+    const route = useRoute();
+    const menuList = getMenuList(static_routes, getCurrentUser().roles);
     return {
       menuList: menuList,
       route,
       store,
     };
   },
-};
+});
 </script>
 
 <style scoped>
